21ce09966461cf61dd791713d936509cb2bdfb49
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-24 16:02:15 +0300

    If blocks

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,13 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        int sayi = 20;
+        if (sayi <20){
+            System.out.println("Sayi yirmiden küçüktür.");
+        }else if (sayi == 20){
+            System.out.println("Sayi yirmiye eşittir.");
+        }else {
+            System.out.println("Sayi yirmiden büyüktür.");
+        }
     }
 }
