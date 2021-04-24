0a2689bc86ff223112b175686272bde8bf1b6be1
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-24 16:36:26 +0300

    array example

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,24 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        String ogrenci1 = "Ahmet";
+        String ogrenci2 = "Mehmet";
+        String ogrenci3 = "Kadir";
+
+        System.out.println(ogrenci1);
+        System.out.println(ogrenci2);
+        System.out.println(ogrenci3);
+
+        System.out.println("---------ARRAYS----------");
+
+        String[] ogrenciler = new String[3];
+
+        ogrenciler[0] = ogrenci1;
+        ogrenciler[1] = ogrenci2;
+        ogrenciler[2] = ogrenci3;
+
+        for (String ogrenci:ogrenciler){
+            System.out.println(ogrenci);
+        }
     }
 }
