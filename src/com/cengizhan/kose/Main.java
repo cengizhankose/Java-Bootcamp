ab0bc2cb24b7cf436c7979113f4ba1264817d413
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-24 16:24:16 +0300

    basic for loop

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,9 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+	    for (int i =1 ; i<=10; i++){
+	        System.out.println(i);
+        }
+	    System.out.println("Döngü bitti");
     }
 }
