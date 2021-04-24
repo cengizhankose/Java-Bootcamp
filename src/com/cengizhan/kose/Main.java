d17cc4b3f6598e9d71dd5d41d3c3811329569cb7
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-24 16:27:49 +0300

    while loop added

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,8 +3,16 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
+        //for loop
 	    for (int i =1 ; i<=10; i++){
 	        System.out.println(i);
+        }
+
+	    int i = 1;
+	    //while loop
+	    while (i<=10){
+	        System.out.println(i);
+	        i++;
         }
 	    System.out.println("Döngü bitti");
     }
