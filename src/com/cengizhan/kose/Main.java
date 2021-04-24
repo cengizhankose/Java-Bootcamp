f957be6fb1dcfff775f1c0e3960a2104902c103e
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-24 16:16:03 +0300

    Greatest num

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,18 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        int sayi1 = 10;
+        int sayi2 = 11;
+        int sayi3 = 12;
+        int enBuyuk = sayi1;
+
+        if (enBuyuk<sayi2){
+            enBuyuk=sayi2;
+        }
+        if (enBuyuk<sayi3){
+            enBuyuk=sayi3;
+        }
+
+        System.out.println("En Büyük Sayı : "+enBuyuk);
     }
 }
