c755a062449c5395d8d610a0c1e817023c5220da
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-26 18:45:21 +0300

    ReCap Demo 2 done

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,18 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+	 double[] myList = {1.2,1.3,2.5,5.6,7.8};
+	 double total = 0 ;
+	 double max = myList[0];
+
+	 for (double num:myList){
+	     if (max<num){
+	         max = num;
+         }
+	     total +=num;
+	     System.out.println(num);
+     }
+	 System.out.println("Toplam  = "+total);
+	 System.out.println("En Büyük  = "+max);
     }
 }
