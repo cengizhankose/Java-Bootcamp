5e6b7781452c8ad3c30b957a5f319e07fc3f2630
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-26 20:29:55 +0300

    done

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,17 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        int number = 29;
+        int total = 1;
+        for (int i = 2; i<number; i++){
+            if (number%i==0){
+                total += i;
+            }
+        }
+        if (total == number){
+            System.out.println("Sayınız mükemmel sayı");
+        }else{
+            System.out.println("Sayınız mükemmel sayı değil");
+        }
     }
 }
