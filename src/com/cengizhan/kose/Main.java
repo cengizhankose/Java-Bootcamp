67b608935174d0eab53e05d7a62149f04aec1969
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-26 18:54:05 +0300

    done

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,21 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        String[][] sehirler  = new String[3][3];
+        sehirler[0][0] = "İstanbul";
+        sehirler[0][1] = "Ankara";
+        sehirler[0][2] = "Kocaeli";
+        sehirler[1][0] = "Edirne";
+        sehirler[1][1] = "Trabzon";
+        sehirler[1][2] = "Eskişehir";
+        sehirler[2][0] = "Sivas";
+        sehirler[2][1] = "Antalya";
+        sehirler[2][2] = "Muğla";
+
+        for (int i = 0 ;i<sehirler.length;i++){
+            for (int j = 0 ;j<sehirler[i].length;j++){
+                System.out.println(sehirler[i][j]);
+            }
+        }
     }
 }
