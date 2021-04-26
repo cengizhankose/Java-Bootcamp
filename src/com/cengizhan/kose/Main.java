86276ca3ef9aea508a2e4694a2929e23d704f888
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-26 19:21:21 +0300

    done

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,20 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        int number = 7919;
+        boolean isPrime = true;
+
+        for (int i=2 ; i<number;i++){
+            if (number % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime){
+            System.out.println(number+" Sayısı Asaldır");
+        }else{
+            System.out.println(number+" Sayısı Asal Değildir");
+        }
     }
 }
