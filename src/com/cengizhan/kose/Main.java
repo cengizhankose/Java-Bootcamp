3bd1efe4928b31979b753f393613ba2021c41484
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-27 00:08:35 +0300

    done

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,20 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        int[] sayilar = new int[]{1,2,3,4,5,6,7,8,9,10,22,34,40};
+        int aranacak = 23254;
+        boolean sayiVar = false;
+
+        for (int sayi : sayilar){
+            if (aranacak==sayi){
+                sayiVar=true;
+                break;
+                }
+        }
+        if (sayiVar){
+            System.out.println(aranacak+" Sayınız dizide bulunmakta");
+        }else{
+            System.out.println(aranacak+" Sayınız dizide bulunmamaktadır");
+        }
     }
 }
