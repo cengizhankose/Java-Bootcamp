55896016366ae4c946ee09516c1303dad7cc9c42
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-04-26 19:46:18 +0300

    done

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,18 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        char harf = 'E';
+
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalın sesli harf");
+                break;
+            default:
+                System.out.println("İnce sesli harf");
+
+        }
     }
 }
