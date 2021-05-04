a3ff9014e3b98fef275cf023a3153f37ece5b154
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-05-04 18:37:17 +0300

    calculator class

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,11 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        DortIslem hesapMakinesi = new DortIslem();
+
+        System.out.println(hesapMakinesi.Topla(3,4));
+        System.out.println(hesapMakinesi.Cikar(4,3));
+        System.out.println(hesapMakinesi.Bol(6,2));
+        System.out.println(hesapMakinesi.Carp(4,2));
     }
 }
