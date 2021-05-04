70be584c3242fc04ab7d4a8cd2034ae9e500d8d9
Cengizhan KÖSE <cengizhan.kose@monsternotebook.com>
2021-05-04 19:04:53 +0300

    Method Overloading

--- src/com/cengizhan/kose/Main.java
+++ src/com/cengizhan/kose/Main.java
@@ -3,6 +3,8 @@ package com.cengizhan.kose;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        DortIslem calculator = new DortIslem();
+        System.out.println(calculator.topla(3,4));
+        System.out.println(calculator.topla(3,4,5));
     }
 }
